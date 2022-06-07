3e300a3d97306b6561439722a7364a8ea099eefc
JSShant <joshua.shanthikumar@outlook.com>
2022-06-07 21:52:35 +1000

    Dark - Theme

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
+let contrastToggle = false
+function toggleContrast() {
+    contrastToggle = !contrastToggle
+    if (contrastToggle) {
+        document.body.classList += " dark-theme"
+    } else {
+        document.body.classList.remove("dark-theme")
+    }
+}
+
+
+
 let isModalOpen = false;
 function toggleModal() {
     if (isModalOpen) {
         isModalOpen = false
-        return document.body.classList.remove("modal--open") 
+        return document.body.classList.remove("modal--open")
     }
     isModalOpen = true
-    document.body.classList += "modal--open"
+    document.body.classList += " modal--open"
+}
+
+function contact(event) {
+    event.preventDefault();
+
+    const loading = document.querySelector(".modal__overlay--loading")
+    const success = document.querySelector(".modal__overlay--success")
+    loading.classList += " modal__overlay--visible"
+    setTimeout(() => {
+        loading.classList.remove("modal__overlay--visible")
+        success.classList += " modal__overlay--visible"
+    }, 1000);
 }
