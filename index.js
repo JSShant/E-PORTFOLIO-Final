6ea572da6e24bc512afe10c1dc341ac3e8851a73
JSShant <joshua.shanthikumar@outlook.com>
2022-06-08 00:46:58 +1000

    Shapes

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
+const scaleFactor = 1 / 20
+function moveBackground(event) {
+    const shapes = document.querySelectorAll(".shape")
+    const x = event.clientX * scaleFactor;
+    const y = event.clientY * scaleFactor;
+    for (let i=0; i < shapes.length; ++i) {
+        const isOdd = i % 2 !== 0;
+        const boolInt = isOdd ? -1 : 1;
+        shapes[i].style.transform= `translate(${x * boolInt}px, ${y * boolInt}px)`
+    }
+}
+
+
+
+
+
+
+
+
 let contrastToggle = false
 function toggleContrast() {
     contrastToggle = !contrastToggle
@@ -7,8 +26,6 @@ function toggleContrast() {
         document.body.classList.remove("dark-theme")
     }
 }
-
-
 
 let isModalOpen = false;
 function toggleModal() {
