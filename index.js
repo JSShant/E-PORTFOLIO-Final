411c0dd47424f1d78999854389218bc10af6feb6
JSShant <joshua.shanthikumar@outlook.com>
2022-07-21 16:17:19 +1000

    Using Email JS to recieve emails from contact page

--- index.js
+++ index.js
@@ -1,50 +1,55 @@
-const scaleFactor = 1 / 20
+const scaleFactor = 1 / 20;
 function moveBackground(event) {
-    const shapes = document.querySelectorAll(".shape")
-    const x = event.clientX * scaleFactor;
-    const y = event.clientY * scaleFactor;
-    for (let i=0; i < shapes.length; ++i) {
-        const isOdd = i % 2 !== 0;
-        const boolInt = isOdd ? -1 : 1;
-        shapes[i].style.transform= `translate(${x * boolInt}px, ${y * boolInt}px)`
-    }
+  const shapes = document.querySelectorAll(".shape");
+  const x = event.clientX * scaleFactor;
+  const y = event.clientY * scaleFactor;
+  for (let i = 0; i < shapes.length; ++i) {
+    const isOdd = i % 2 !== 0;
+    const boolInt = isOdd ? -1 : 1;
+    shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`;
+  }
 }
 
-
-
-
-
-
-
-
-let contrastToggle = false
+let contrastToggle = false;
 function toggleContrast() {
-    contrastToggle = !contrastToggle
-    if (contrastToggle) {
-        document.body.classList += " dark-theme"
-    } else {
-        document.body.classList.remove("dark-theme")
-    }
+  contrastToggle = !contrastToggle;
+  if (contrastToggle) {
+    document.body.classList += " dark-theme";
+  } else {
+    document.body.classList.remove("dark-theme");
+  }
 }
 
 let isModalOpen = false;
 function toggleModal() {
-    if (isModalOpen) {
-        isModalOpen = false
-        return document.body.classList.remove("modal--open")
-    }
-    isModalOpen = true
-    document.body.classList += " modal--open"
+  if (isModalOpen) {
+    isModalOpen = false;
+    return document.body.classList.remove("modal--open");
+  }
+  isModalOpen = true;
+  document.body.classList += " modal--open";
 }
 
 function contact(event) {
-    event.preventDefault();
-
-    const loading = document.querySelector(".modal__overlay--loading")
-    const success = document.querySelector(".modal__overlay--success")
-    loading.classList += " modal__overlay--visible"
-    setTimeout(() => {
-        loading.classList.remove("modal__overlay--visible")
-        success.classList += " modal__overlay--visible"
-    }, 1000);
+  event.preventDefault();
+  const loading = document.querySelector(".modal__overlay--loading");
+  const success = document.querySelector(".modal__overlay--success");
+  loading.classList += " modal__overlay--visible";
+  emailjs
+    .sendForm(
+      "service_q8dxe54",
+      "template_1x3i6m9",
+      event.target,
+      "FxOlAJu8N7n00174f"
+    )
+    .then(() => {
+      loading.classList.remove("modal__overlay--visible");
+      success.classList += " modal__overlay--visible";
+    })
+    .catch(() => {
+      loading.classList.remove("modal__overlay--visible");
+      alert(
+        "The email service is temporarily unavaible. Please contact me directly on joshuashantportfolio@gmail.com"
+      );
+    });
 }
